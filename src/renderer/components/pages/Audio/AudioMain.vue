e60a6d7a29f02f0278b951230f9f9d541ff69882
taka <taka05061@gamil.com>
2020-03-27 01:06:17 +0900

    add:再生機能の追加

--- src/renderer/components/pages/Audio/AudioMain.vue
+++ src/renderer/components/pages/Audio/AudioMain.vue
@@ -13,10 +13,10 @@
       <div class="seek-bar">
         <input
           type="range"
-          v-model="musicTime"
+          v-model="seekTime"
           class="custom-range"
           min="0"
-          :max="musicEndTime"
+          :max="seekEndTime"
           step="1"
         />
       </div>
@@ -40,45 +40,57 @@ export default {
   name: 'AudioMain',
   data () {
     return {
-      time: null,
-      audioIndex: 0,
-      status: false,
+      audioIndex: 0, // プレイリストの添え字
+      status: false, // 再生状態の確認
+      music: null, // Audioインスタンス
       // ダミー
       playList: [
-        {title: 'Hello Electron', endTime: 180},
-        {title: 'Hello Vue', endTime: 200},
-        {title: 'Hello End', endTime: 320}
+        {title: '野良猫は宇宙を目指した', path: require('@/assets/cat.mp3')},
+        {title: 'morning', path: require('@/assets/morning.mp3')},
+        {title: '不老不死', path: require('@/assets/不老不死.mp3')}
       ],
-      musicTime: 0, // 現在の進行度
-      formatEndTime: '',
-      formatTime: ''
+      formatTime: '', // 現在の再生時間
+      formatEndTime: '', // 曲の総時間
+      seekEndTime: 0, // シークバーの最大値
+      seekTime: 0 // 現在のシークバーの現在位置(再生位置)
     }
   },
-  created () {
-    this.init()
-  },
   watch: {
-    // 停止中でもシークバーの値を反映させる
-    musicTime () {
-      this.formatTime = this.format(this.musicTime)
+    seekTime () {
+      // ユーザが操作した時のみ適用(再生時間の視覚表示にも使ってるためjs側からの操作を弾く必要がある)
+      // 現在の再生位置よりもシークバーの位置(秒数と読み替えても可)が大きいもしくは小さければ適用
+      // (戻る,進むがあるので両方考慮)
+      if (Math.floor(this.music.currentTime) !== this.seekTime) {
+        this.music.currentTime = this.seekTime
+      }
     }
   },
   computed: {
-    musicEndTime () {
-      return this.playList[this.audioIndex].endTime
-    },
     musicTitle () {
       return this.playList[this.audioIndex].title
+    },
+    musicPath () {
+      return this.playList[this.audioIndex].path
     }
+  },
+  mounted () {
+    this.init()
   },
   methods: {
     init () {
       // 初期化
-      this.stopMusic()
-      this.musicTime = 0
-      this.formatTime = this.format(this.musicTime)
+      if (this.status) this.stopMusic()
+      // シークバーの初期化
+      this.seekTime = 0
+      // 現在の再生時間を初期化
+      this.formatTime = this.format(0)
+      // 曲のあるパスをセット
+      this.music = new Audio(this.musicPath)
       // 曲の終わり時間の代入
-      this.formatEndTime = this.format(this.musicEndTime)
+      this.music.addEventListener('loadedmetadata', () => {
+        this.seekEndTime = Math.floor(this.music.duration)
+        this.formatEndTime = this.format(this.seekEndTime)
+      })
     },
     next () {
       this.audioIndex++
@@ -96,18 +108,22 @@ export default {
     },
     startMusic () {
       this.status = true
-      // 現在の曲の進行
-      this.time = setInterval(() => {
-        this.musicTime++
-        this.formatTime = this.format(this.musicTime)
-        if (this.musicTime === this.musicEndTime) this.stopMusic()
-      }, 1000)
+      this.music.play()
+      // 再生時間の取得
+      // 再生中
+      this.music.addEventListener('timeupdate', () => {
+        this.seekTime = Math.floor(this.music.currentTime)
+        this.formatTime = this.format(this.seekTime)
+      })
+      // 曲の終了
+      this.music.addEventListener('ended', () => {
+        this.stopMusic()
+      })
     },
     stopMusic () {
       this.status = false
-      clearInterval(this.time)
+      this.music.pause()
     },
-    // 時間をフォーマット
     format (seconds) {
       const minute = seconds !== 0 ? Math.floor(seconds / 60) : 0
       const second = seconds !== 0 ? seconds - minute * 60 : 0
@@ -143,7 +159,7 @@ input[type='range']::-webkit-slider-thumb {
   display: inline-block;
   background-color: rgba(0, 0, 0, 0);
   border: none;
-  color: gray;
+  color: ghostwhite;
   font-size: 20px;
 }
 </style>
